62aab9c19db8903615ef28c190f0b52f7d1f2a29
Waldemar <itzm1lky@yandex.ru>
2022-12-22 02:31:03 +0300

    add filter

--- djangoreact/api/views.py
+++ djangoreact/api/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from .models import Vacancies
 from .serializers import VacanciesSerializer
@@ -6,3 +7,5 @@ from .serializers import VacanciesSerializer
 class VacanciesViewSet(viewsets.ModelViewSet):
     queryset = Vacancies.objects.all()
     serializer_class = VacanciesSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'company']
