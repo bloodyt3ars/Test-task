1154d8e6166a92bade11ac96ea4e8dc958cc48a9
Waldemar <itzm1lky@yandex.ru>
2022-12-27 02:12:30 +0300

    Итоговый проект

--- djangoreact/djangoreact/settings.py
+++ djangoreact/djangoreact/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-47l*8h8758t*k*)+i=q)w18e9i%1pj%-p%^82+hrmhm5j-(!g4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', '127.0.0.1:8001', '127.0.0.1:8002']
+ALLOWED_HOSTS = 'localhost 127.0.0.1 [::1]'.split(" ")
 
 
 # Application definition
